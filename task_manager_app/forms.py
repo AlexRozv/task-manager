acf4452c1d4c97143d3e652a680eae9162aad38f
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-16 11:56:51 +0300

    added forms.py with custom forms

---
+++ task_manager_app/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from task_manager_app.models import Worker, Task
+
+
+class WorkerCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        for field_name in ["username", "password1", "password2"]:
+            self.fields[field_name].help_text = None
+
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "position"
+        )
+
+
+class UpdateTaskAssigneesForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Change assignees"
+    )
+
+    class Meta:
+        model = Task
+        fields = ("assignees", )
