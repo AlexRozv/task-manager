a14435e9280601f94bca4682f12c4a40a3eb3614
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-18 15:18:11 +0300

    added models to admin panel

--- task_manager_app/admin.py
+++ task_manager_app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_manager_app.models import Worker, Position, Task, TaskType
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "position")
+    ordering = ("username",)
+    list_filter = ("position",)
+    search_fields = ("username",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    ordering = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    ordering = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "deadline",
+        "is_completed",
+        "priority",
+        "task_type",
+    )
+    ordering = ("name", "deadline", )
+    list_filter = ("task_type", "priority", "is_completed")
+    search_fields = ("name", "description", )
