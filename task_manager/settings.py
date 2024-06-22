dfe2d3e68c1259945e1cd0b3b9d05cd002d49ed3
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-06-22 14:06:02 +0300

    fix settings hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "mysite-g74a.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost",]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
